ebace817ddea8c52beb208c93b5b8068229caa75
Nikhil Amin <aminnikhil073@gmail.com>
2018-01-21 15:36:52 +0530

    Update SourceCodeExtracter.py

--- SourceCodeExtracter.py
+++ SourceCodeExtracter.py
@@ -1,3 +1,6 @@
+#Nikhil Amin -:- https://github.com/nikhilamin073
+#Python Script to extract source code of the entered URL to a text file.
+
 import urllib
 
 url = raw_input("\nEnter a URL to extract source code: ")
