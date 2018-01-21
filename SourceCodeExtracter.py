d9a8b4f1111a89cb4769ccbeb1815503e2ca6586
Nikhil Amin <aminnikhil073@gmail.com>
2018-01-21 15:05:23 +0530

    Add files via upload

---
+++ SourceCodeExtracter.py
@@ -0,0 +1,11 @@
+import urllib
+
+url = raw_input("\nEnter a URL to extract source code: ")
+type(url)						#user input for URL
+
+ofile = raw_input("\nEnter a Output filename to save the extracted source code (.txt recommended): ")
+type(ofile)						#user input for output file name
+
+urllib.urlretrieve(url, ofile)  			#using urlretrieve to extract source code and store it in a file.
+
+print ("\nSource code of '" + url + "' has been sucessfully stored in '" + ofile + "' file.")
